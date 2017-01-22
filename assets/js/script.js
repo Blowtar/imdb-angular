b5921c3787e0a2e7514246e69ba643b918f2b087
Lee Easeman <lee.easeman@outlook.com>
2017-01-22 23:28:54 +0000

    Completed the simple Angular movie app.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,15 @@
 'use strict';
 
-angular.module('movieApp', []).controller('MovieController', function($scope, $http) {
+var movieReviewApi = angular.module('movieApp', []);
+
+movieReviewApi.controller('MovieController', function($scope, $http) {
   
   $scope.$watch('search', function() {
     fetch();
   });
 
   // Default search result
-  $scope.search = "Sherlock Holmes";
+  $scope.search = "Frozen";
 
   function fetch() {
     
@@ -23,10 +25,28 @@ angular.module('movieApp', []).controller('MovieController', function($scope, $h
 
   $scope.update = function(movie) {
     $scope.search = movie.Title;
+    $scope.search = movie.Details;
+    $scope.search = movie.Poster
   };
 
   $scope.select = function() {
     this.setSelectionRange(0, this.value.length);
   }
-    
+  
+  // Review Function
+  $scope.todoList = [{todoText:'Harry Potter and the Gobshite of Fire was amazing...', done:false}];
+
+  $scope.todoAdd = function() {
+      $scope.todoList.push({todoText:$scope.todoInput, done:false});
+      $scope.todoInput = "";
+  };
+
+  $scope.remove = function() {
+      var oldList = $scope.todoList;
+      $scope.todoList = [];
+      angular.forEach(oldList, function(x) {
+          if (!x.done) $scope.todoList.push(x);
+      });
+  };
+  
 });
